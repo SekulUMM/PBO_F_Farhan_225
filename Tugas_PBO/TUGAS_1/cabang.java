36ea09ab1bbe153ceca9b4c2cc422af4b7634273
Mochammad Farhan Hikmadi <saylmly@gmail.com>
2025-03-07 13:37:27 +0700

    Pembaruan baru

--- Tugas_PBO/TUGAS_1/cabang.java
+++ Tugas_PBO/TUGAS_1/cabang.java
@@ -1,24 +1,34 @@
-package Codelab.Modul_1.TUGAS_1;
+//tujuan file
+package Tugas_PBO.TUGAS_1;
 
+//fungsi
 import java.util.Scanner;
 
-public class Percabangan {
-    public static void main(String[] args) {
+//class name percabangan
+public class cabang {
+    //menggunakan static void sebagai default
+    public static void main (String[] args) {
+        //membuat line untuk pengisian input
         Scanner scanner = new Scanner(System.in);
 
+        //tampilan menu yang akan di pilih
         System.out.println("Pilih login:");
         System.out.println("1. Admin");
         System.out.println("2. Mahasiswa");
         System.out.print("Masukkan pilihan: ");
+        //menggunakan integer pilihan 1 dan juga 2
         int pilihan = scanner.nextInt();
+        //menghilangkan buffer
         scanner.nextLine();
 
+        //pilihan jika memilih "admin"
         if (pilihan == 1) {
             System.out.print("Masukkan username: ");
             String username = scanner.nextLine();
             System.out.print("Masukkan password: ");
             String password = scanner.nextLine();
 
+            //penentuan untuk login admin user
             if (username.startsWith("Admin225") && password.startsWith("Password225")) {
                 String nimAkhir = username.substring(5);
                 if (password.equals("Password" + nimAkhir)) {
@@ -29,12 +39,15 @@ public class Percabangan {
             } else {
                 System.out.println("Login gagal! Username atau password salah.");
             }
+
+            //pilihan jika memilih "mahasiswa"
         } else if (pilihan == 2) {
             System.out.print("Masukkan Nama: ");
             String nama = scanner.nextLine();
             System.out.print("Masukkan NIM: ");
             String nim = scanner.nextLine();
 
+            //penentuan untuk login mahasiswa user
             if (nama.equals("Mochammad Farhan Hikmadi") && nim.equals("202410370110225")) {
                 System.out.println("Login Mahasiswa berhasil!");
                 System.out.println("Nama: " + nama);
@@ -46,6 +59,7 @@ public class Percabangan {
             System.out.println("Pilihan tidak valid.");
         }
 
+        //menutup scanner agar aman
         scanner.close();
     }
 }
